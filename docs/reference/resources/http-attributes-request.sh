0c3999515cdc3907a0d9409a12f3833e78560ac9
serhii <stitenko@thingsboard.io>
2021-01-15 13:55:45 +0200

    refactoring

--- docs/reference/resources/http-attributes-request.sh
+++ docs/reference/resources/http-attributes-request.sh
@@ -7,4 +7,4 @@ curl -v -X GET "https://thingsboard.cloud/api/v1/$ACCESS_TOKEN/attributes?client
 # for local ThingsBoard
 
 # Send HTTP attributes request
-curl -v -X GET http://localhost:8080/api/v1/$ACCESS_TOKEN/attributes?clientKeys=attribute1,attribute2&sharedKeys=shared1,shared2
+curl -v -X GET "http://localhost:8080/api/v1/$ACCESS_TOKEN/attributes?clientKeys=attribute1,attribute2&sharedKeys=shared1,shared2"
